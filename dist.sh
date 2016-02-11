1d7086c7cea1dfef01e3412601dd5c6ae577f2e1
Riccardo Metere <rick@metere.it>
2016-02-11 02:30:58 +0000

    fixed minor issues

--- dist.sh
+++ dist.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
 python setup.py bdist_wheel --universal
-twine upload dist/*
-
-git filter-branch --force --index-filter 'git rm --cached --ignore-unmatch .pypirc' --prune-empty --tag-name-filter cat -- --all
+twine upload dist/* --config-file .pypirc
